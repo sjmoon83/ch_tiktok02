3463d0bd0aa6ca0765ffdc458dfb0ce477a5dee1
sjmoon83 <chachaprint@naver.com>
2023-09-09 00:04:02 +0900

    230909_photo

--- lib/features/name_navigation/widgets/write_screen.dart
+++ lib/features/name_navigation/widgets/write_screen.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:tiktok_clone/constants/gaps.dart';
 import 'package:tiktok_clone/constants/sizes.dart';
+import 'package:tiktok_clone/features/videos/video_recording_screen.dart';
 
 class WriteScreen extends StatefulWidget {
   const WriteScreen({super.key});
@@ -12,6 +14,14 @@ class WriteScreen extends StatefulWidget {
 class _WriteScreenState extends State<WriteScreen> {
   void _cancelTap() {
     Navigator.of(context).pop();
+  }
+
+  void _onRecordingTap() {
+    Navigator.of(context).push(
+      MaterialPageRoute(
+        builder: (context) => const VideoRecordingScreen(),
+      ),
+    );
   }
 
   @override
@@ -33,74 +43,56 @@ class _WriteScreenState extends State<WriteScreen> {
           ),
         ),
       ),
-      body: ListView(
-        children: [
-          Padding(
-            padding: const EdgeInsets.all(12.0),
-            child: Column(
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            const Column(
               children: [
-                const Row(
-                  children: [
-                    CircleAvatar(
-                      backgroundImage: NetworkImage(
-                          'https://avatars.githubusercontent.com/u/3612017?v=4'),
-                    ),
-                    Gaps.h10,
-                    SizedBox(
-                      width: 310,
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                            children: [
-                              Text('data'),
-                              Row(
-                                children: [
-                                  Text('2m'),
-                                  Gaps.h5,
-                                  Icon(
-                                    Icons.more_horiz,
-                                  ),
-                                ],
-                              ),
-                            ],
-                          ),
-                          Text('data'),
-                        ],
-                      ),
-                    ),
-                  ],
+                CircleAvatar(
+                  radius: Sizes.size24,
+                  backgroundImage: NetworkImage(
+                      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQRqanvbMaSXKOKtAR51_yX57oliEniGHqgeMKbxzxF6pGi5gVNZ_n9pxf_h1b546_G7DY&usqp=CAU'),
                 ),
-                Row(
-                  children: [
-                    Container(
-                      padding: const EdgeInsets.symmetric(
-                        horizontal: Sizes.size6,
-                      ),
-                    ),
-                    const Text(
-                      '|',
-                      style: TextStyle(
-                        backgroundColor: Colors.grey,
-                        color: Colors.grey,
-                        fontSize: 100,
-                      ),
-                    ),
-                    Gaps.h28,
-                    Container(
-                      width: 250,
-                      decoration: const BoxDecoration(),
-                      child: Image.network(
-                        'https://i.namu.wiki/i/oU0avPQmlPv0p13BPnuEqyzmtGl9SoTArdKVYpb1r5CYXrpUjEqtiurvlFDjpXdOMyDXwIFYpz0x3PgtS92_8A.webp',
-                      ),
-                    ),
-                  ],
+                SizedBox(
+                  height: 120,
+                  child: VerticalDivider(
+                    color: Colors.red,
+                  ),
+                ),
+                CircleAvatar(
+                  radius: Sizes.size14,
+                  backgroundImage: NetworkImage(
+                      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQRqanvbMaSXKOKtAR51_yX57oliEniGHqgeMKbxzxF6pGi5gVNZ_n9pxf_h1b546_G7DY&usqp=CAU'),
                 ),
               ],
             ),
-          ),
-        ],
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const Text('Pooh'),
+                Gaps.v8,
+                const SizedBox(
+                  width: 250,
+                  child: Text(
+                      'Pooh chi pooh pooh chi pooh pooh chi pooh pooh chi Pooh chi pooh pooh chi pooh pooh chi pooh pooh chi'),
+                ),
+                Gaps.v8,
+                GestureDetector(
+                  onTap: _onRecordingTap,
+                  child: const FaIcon(
+                    FontAwesomeIcons.paperclip,
+                  ),
+                ),
+              ],
+            ),
+            const Icon(
+              Icons.close,
+            ),
+          ],
+        ),
       ),
     );
   }
---
+++ lib/features/videos/video_recording_screen.dart
@@ -0,0 +1,127 @@
+import 'package:camera/camera.dart';
+import 'package:flutter/material.dart';
+import 'package:permission_handler/permission_handler.dart';
+import 'package:tiktok_clone/constants/gaps.dart';
+import 'package:tiktok_clone/constants/sizes.dart';
+
+class VideoRecordingScreen extends StatefulWidget {
+  const VideoRecordingScreen({super.key});
+
+  @override
+  State<VideoRecordingScreen> createState() => _VideoRecordingScreenState();
+}
+
+class _VideoRecordingScreenState extends State<VideoRecordingScreen> {
+  bool _hasPermission = false;
+
+  bool _isSelfieMode = false;
+
+  late FlashMode _flashMode;
+
+  late CameraController _cameraController;
+
+  Future<void> initCamera() async {
+    final cameras = await availableCameras();
+
+    if (cameras.isEmpty) {
+      return;
+    }
+
+    _cameraController = CameraController(
+        cameras[_isSelfieMode ? 1 : 0], ResolutionPreset.ultraHigh);
+
+    await _cameraController.initialize();
+
+    _flashMode = _cameraController.value.flashMode;
+  }
+
+  Future<void> initPermissions() async {
+    final cameraPermission = await Permission.camera.request();
+    final micPermission = await Permission.microphone.request();
+
+    final cameraDenied =
+        cameraPermission.isDenied || cameraPermission.isPermanentlyDenied;
+
+    final micDenied =
+        micPermission.isDenied || micPermission.isPermanentlyDenied;
+
+    if (!cameraDenied && !micDenied) {
+      _hasPermission = true;
+      await initCamera();
+      setState(() {});
+    }
+  }
+
+  Future<void> _setFlashMode(FlashMode newFlashMode) async {
+    await _cameraController.setFlashMode(newFlashMode);
+    _flashMode = newFlashMode;
+    setState(() {});
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    initPermissions();
+  }
+
+  Future<void> _toggleSelfieMode() async {
+    _isSelfieMode = !_isSelfieMode;
+    await initCamera();
+    setState(() {});
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        backgroundColor: Colors.black,
+        body: SizedBox(
+          width: MediaQuery.of(context).size.width,
+          child: !_hasPermission || !_cameraController.value.isInitialized
+              ? const Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      "Initializing...",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: Sizes.size20,
+                      ),
+                    ),
+                    Gaps.v20,
+                    CircularProgressIndicator.adaptive(),
+                  ],
+                )
+              : Stack(
+                  alignment: Alignment.center,
+                  children: [
+                    CameraPreview(_cameraController),
+                    Positioned(
+                      bottom: Sizes.size20,
+                      child: Row(
+                        children: [
+                          IconButton(
+                            color: _flashMode == FlashMode.off
+                                ? Colors.amber
+                                : Colors.black,
+                            onPressed: () => _setFlashMode(FlashMode.off),
+                            icon: const Icon(
+                              Icons.flash_off_outlined,
+                            ),
+                          ),
+                          Gaps.v10,
+                          IconButton(
+                            color: Colors.black,
+                            onPressed: _toggleSelfieMode,
+                            icon: const Icon(
+                              Icons.cameraswitch,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+        ));
+  }
+}
